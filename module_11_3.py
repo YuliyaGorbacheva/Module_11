185b3a432bea1cd88a751c9304486376cd57886f
YuliyaGorbacheva <Ulechka782@yandex.ru>
2025-01-12 12:38:06 +0400

    Add files via upload

--- module_11_3.py
+++ module_11_3.py
@@ -1,11 +1,9 @@
 
-
 def introspection_info(obj):
     obj_type = type(obj).__name__
-    attributes = dir(obj)
-    methods = [method for method in attributes if callable(getattr(obj, method))]
+    attributes = [attribute for attribute in dir(obj) if not callable(getattr(obj, attribute))]
+    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
     module = obj.__class__.__module__
-
     info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module}
     return info
 
